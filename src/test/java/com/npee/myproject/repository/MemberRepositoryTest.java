84267c8bc6b14403ed5d6f173392f01b8dce81ad
npee <jmc4664@gmail.com>
2021-01-28 00:04:24 +0900

    Spring Data JPA의 Repository interface 이용하여 구현, 테스트

---
+++ src/test/java/com/npee/myproject/repository/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.npee.myproject.repository;
+
+import com.npee.myproject.domain.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId())
+                .isEqualTo(member.getId());
+        assertThat(findMember.getUsername())
+                .isEqualTo(member.getUsername());
+        assertThat(findMember)
+                .isEqualTo(member);
+
+    }
+
+}
