1f9a6d9564c2bd806214146607807b39dafa5970
npee <jmc4664@gmail.com>
2021-01-27 23:14:03 +0900

    JUint 동작 테스트

--- src/main/java/com/npee/myproject/controller/HelloController.java
+++ src/main/java/com/npee/myproject/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.npee.myproject.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
