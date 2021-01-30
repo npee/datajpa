647cc684ba322447f9d7985dbfb39ffec6dfda0e
npee <jmc4664@gmail.com>
2021-01-30 12:55:11 +0900

    기본 CRUD 테스트

--- src/test/java/com/npee/myproject/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/npee/myproject/repository/MemberJpaRepositoryTest.java
@@ -2,11 +2,14 @@ package com.npee.myproject.repository;
 
 import com.npee.myproject.domain.entity.Member;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Java6AbstractBDDSoftAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,7 +23,7 @@ class MemberJpaRepositoryTest {
     MemberJpaRepository memberJpaRepository;
 
     @Test
-     public void testMember() {
+    public void testMember() {
         Member member = new Member("memberA");
         Member savedMember = memberJpaRepository.save(member);
 
@@ -32,6 +35,31 @@ class MemberJpaRepositoryTest {
                 .isEqualTo(member.getUsername());
         assertThat(findMember)
                 .isEqualTo(member);
-
     }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2L);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(0L);
+    }
+
+
 }
