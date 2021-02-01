e20b734f750a17dcdeb32214cbfcdc483420c6b7
npee <jmc4664@gmail.com>
2021-02-01 22:47:22 +0900

    Spring Data JPA 동작 테스트(기존 JPA 코드 붙여넣기)

--- src/test/java/com/npee/myproject/repository/MemberRepositoryTest.java
+++ src/test/java/com/npee/myproject/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,6 +36,35 @@ class MemberRepositoryTest {
         assertThat(findMember)
                 .isEqualTo(member);
 
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        member1.setUsername("member3"); // Transaction 내에 있으므로 변경 감지 일어남
+
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2L);
+
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2L);
+
+//        memberJpaRepository.delete(member1);
+//        memberJpaRepository.delete(member2);
+//
+//        long resCount = memberJpaRepository.count();
+//        assertThat(resCount).isEqualTo(0L);
     }
 
 }
