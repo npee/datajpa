5f6f2b00fc4cb8f1a538ec509807c2cc3b7b042b
npee <jmc4664@gmail.com>
2021-02-18 22:57:34 +0900

    Spring Data JPA 지연 로딩 전략 N + 1 문제 발생 테스트

--- src/test/java/com/npee/myproject/repository/MemberRepositoryTest.java
+++ src/test/java/com/npee/myproject/repository/MemberRepositoryTest.java
@@ -14,6 +14,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -32,6 +34,9 @@ class MemberRepositoryTest {
 
     @Autowired
     TeamRepository teamRepository;
+    
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     public void testMember() {
@@ -254,6 +259,33 @@ class MemberRepositoryTest {
 
         // then
         assertThat(resultCount).isEqualTo(3);
+    }
+
+    @Test
+    public void findMemberLazy() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // member:team -> lazy loading
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> members = memberRepository.findAll();
+
+        // N + 1
+        for (Member member : members) {
+            System.out.println("member.getUsername() = " + member.getUsername());
+            System.out.println("member.getTeam().getName() = " + member.getTeam().getName());
+        }
+
     }
 
 }
