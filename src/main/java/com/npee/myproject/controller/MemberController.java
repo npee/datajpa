276b127427c8b0fb682f32cadf214906310c5d4b
npee <jmc4664@gmail.com>
2021-02-21 20:36:11 +0900

    JPA 도메인 클래스 컨버터 비교 - 단순한 쿼리에 적용

---
+++ src/main/java/com/npee/myproject/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.npee.myproject.controller;
+
+import com.npee.myproject.domain.entity.Member;
+import com.npee.myproject.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
