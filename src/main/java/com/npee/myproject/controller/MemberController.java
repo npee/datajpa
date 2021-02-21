fd30cfb42656036a5b85ab964cc8cf86d33dfb5c
npee <jmc4664@gmail.com>
2021-02-22 01:40:24 +0900

    JPA 페이징, 정렬 DTO 리턴, 코드 최적화(Page.map())

--- src/main/java/com/npee/myproject/controller/MemberController.java
+++ src/main/java/com/npee/myproject/controller/MemberController.java
@@ -1,10 +1,12 @@
 package com.npee.myproject.controller;
 
 import com.npee.myproject.domain.entity.Member;
+import com.npee.myproject.domain.entity.dto.MemberTeamDto;
 import com.npee.myproject.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,14 +31,15 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    public Page<Member> list(Pageable pageable) {
-        return memberRepository.findAll(pageable);
+    public Page<MemberTeamDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberTeamDto::new);
     }
 
     @PostConstruct
     public void init() {
         for (int i = 1; i < 100; i++) {
-            memberRepository.save(new Member("user" + i));
+            memberRepository.save(new Member("user" + i, i, null));
         }
     }
 }
--- src/main/java/com/npee/myproject/domain/entity/dto/MemberTeamDto.java
+++ src/main/java/com/npee/myproject/domain/entity/dto/MemberTeamDto.java
@@ -1,5 +1,6 @@
 package com.npee.myproject.domain.entity.dto;
 
+import com.npee.myproject.domain.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,4 +10,11 @@ public class MemberTeamDto {
     private Long id;
     private String username;
     private String teamName;
+
+    public MemberTeamDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+        if (!(member.getTeam() == null))
+            this.teamName = member.getTeam().getName();
+    }
 }
