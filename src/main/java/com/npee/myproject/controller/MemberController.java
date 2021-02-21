630ca40a2240b85948cf543fd6d026654d9607de
npee <jmc4664@gmail.com>
2021-02-22 01:19:53 +0900

    JPA 페이징, 정렬 기능 사용

--- src/main/java/com/npee/myproject/controller/MemberController.java
+++ src/main/java/com/npee/myproject/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.npee.myproject.controller;
 import com.npee.myproject.domain.entity.Member;
 import com.npee.myproject.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +28,15 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 1; i < 100; i++) {
+            memberRepository.save(new Member("user" + i));
+        }
     }
 }
