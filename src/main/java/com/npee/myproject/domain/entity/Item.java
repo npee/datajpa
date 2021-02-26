5a0adf181fcd6e0f61156d0be6414e8d7f551d00
npee <jmc4664@gmail.com>
2021-02-26 11:50:25 +0900

    Spring Data JPA persist/merge 판단기준 테스트 - 임의의 ID를 사용하게 될 경우 Persistable 구현

--- src/main/java/com/npee/myproject/domain/entity/Item.java
+++ src/main/java/com/npee/myproject/domain/entity/Item.java
@@ -3,15 +3,17 @@ package com.npee.myproject.domain.entity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Item {
+public class Item implements Persistable<String> {
 
     @Id
     // @GeneratedValue
@@ -19,5 +21,16 @@ public class Item {
 
     public Item(String id) {
         this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        // 로직 작
+        return false;
     }
 }
