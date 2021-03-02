1fb81245cbdb63668704566b4ebc81aac9a0ffa5
npee <jmc4664@gmail.com>
2021-03-02 22:15:31 +0900

    Spring Data JPA QueryByExample test

--- src/test/java/com/npee/myproject/repository/MemberRepositoryTest.java
+++ src/test/java/com/npee/myproject/repository/MemberRepositoryTest.java
@@ -7,10 +7,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -341,5 +338,32 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findAll(spec);
         Assertions.assertThat(result.size()).isEqualTo(1);
     }
+
+    @Test
+    public void queryByExample() {
+        // given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        // Probe
+        Member member = new Member("m1");
+        Example<Member> example = Example.of(member);
+
+        List<Member> result = memberRepository.findAll(example);
+
+        Assertions.assertThat(result.get(0).getUsername()).isEqualTo("m1");
+
+    }
+
 
 }
