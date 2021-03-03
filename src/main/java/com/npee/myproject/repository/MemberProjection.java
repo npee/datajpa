8c7adebe067e8e94ee26a57c6b9f2d09f1b2b9e4
npee <jmc4664@gmail.com>
2021-03-04 00:44:27 +0900

    Spring Data JPA NativeQuery - Projection 활용

---
+++ src/main/java/com/npee/myproject/repository/MemberProjection.java
@@ -0,0 +1,7 @@
+package com.npee.myproject.repository;
+
+public interface MemberProjection {
+    Long getId();
+    String getUsername();
+    String getTeamName();
+}
--- src/test/java/com/npee/myproject/repository/MemberRepositoryTest.java
+++ src/test/java/com/npee/myproject/repository/MemberRepositoryTest.java
@@ -406,8 +406,12 @@ class MemberRepositoryTest {
         em.clear();
 
         // when
-        Member result = memberRepository.findByNativeQuery("m1");
-        System.out.println("result = " + result);
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = result.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection = " + memberProjection.getUsername());
+            System.out.println("memberProjection = " + memberProjection.getTeamName());
+        }
     }
 
 }
