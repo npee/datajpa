a2d379a39e68915a2e6c5211a2ac72b59d4d5b13
npee <jmc4664@gmail.com>
2021-03-03 23:42:23 +0900

    Spring Data JPA Projection - Open Projection

--- src/main/java/com/npee/myproject/repository/UserNameOnly.java
+++ src/main/java/com/npee/myproject/repository/UserNameOnly.java
@@ -1,5 +1,9 @@
 package com.npee.myproject.repository;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface UserNameOnly {
+
+    @Value("#{target.username + ' ' + target.age}")
     String getUsername();
 }
